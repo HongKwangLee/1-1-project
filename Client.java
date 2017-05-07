c3a20e6a53886eabc7cdb6fb989a07174e27565b
이홍광 <rhkstkdlftkr@naver.com>
2017-05-07 19:50:49 +0900

    업데이트

--- Client.java
+++ Client.java
@@ -1,6 +1,8 @@
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,18 +10,23 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.html.HTML;
 
-public class Client extends JFrame implements ActionListener{
+public class Client extends JFrame implements ActionListener, KeyListener {
 	
 	private JPanel contentPane;
 	private JTextField tf_ip;
@@ -28,21 +35,160 @@ public class Client extends JFrame implements ActionListener{
 	private JButton sv_join = new JButton("서버 접속");
 	
 	private JPanel contentPane2;
+	private JTextArea textArea = new JTextArea();
+	private JScrollPane scrollPane = new JScrollPane(textArea);
 	private JTextField textField;
 	private JButton msg_enter = new JButton("전송");
+	private JList list = new JList();
+	private JScrollPane scrollPane2 = new JScrollPane(list);
 	
+	private Socket sc;
+	private String Ip = "";
+	private int port;
+	private String nickname = "";
+	
+	private InputStream is;
+	private OutputStream os;
+	private DataInputStream dis;
+	private DataOutputStream dos;
+	private StringTokenizer st;
+	
+	Vector user_list = new Vector();
+	
+	public void Start(){
+		try {
+			sc = new Socket(Ip, port); // 서버 접속
+			
+			if(sc != null){
+				access();
+			}
+		} catch (UnknownHostException e) {
+			JOptionPane.showMessageDialog(null, "연결실패", "알림", JOptionPane.ERROR_MESSAGE);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "연결실패", "알림", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public void access(){
+		try {		
+			is = sc.getInputStream();
+			os = sc.getOutputStream();
+			dis = new DataInputStream(is);
+			dos = new DataOutputStream(os);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "스트림 에러", "알림", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		SendMsg(nickname);
+		
+		user_list.add(nickname);
+		
+		Thread th = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while(true){
+					try {
+						String msg = dis.readUTF();
+						
+						st = new StringTokenizer(msg, "/");
+						
+						String pt = st.nextToken();
+						String Msg = st.nextToken();
+						
+						if(pt.equals("NewUser")){
+							user_list.add(Msg);
+							textArea.append(Msg+"님이 입장하셨습니다.\n");
+						}else if(pt.equals("OldUser")){
+							user_list.add(Msg);
+						}else if(pt.equals("Whisper")){
+							textArea.append(Msg+"\n");
+							scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
+						}else if(pt.equals("UserOut")){
+							user_list.remove(Msg);
+						}else if(pt.equals("setListData")){
+							list.setListData(user_list);
+						}else{
+							textArea.append(pt+":"+Msg+"\n");
+							scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
+						}
+						
+					} catch (IOException e) {
+						try {
+							is.close();
+							os.close();
+							dis.close();
+							dos.close();
+							sc.close();
+							JOptionPane.showMessageDialog(null, "서버 연결 에러", "알림", JOptionPane.ERROR_MESSAGE);
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						break;
+					}
+				}
+				
+			}
+		});
+		th.start();
+	}
+	
+	
+	// sendmsg
+	public void SendMsg(String msg){
+		try {
+			dos.writeUTF(msg);
+		} catch (IOException e) {
+			e.printStackTrace(); // 메세지 보넬때 오류 = 스트림 오류 알빠아님(맞나?)
+		}
+	}
 	
 	Client(){
-		dp(); //화면
+		dp(); //접속 화면
 		actions();
 	}
 	
+	
+	// 엑션들추가
 	public void actions(){
 		sv_join.addActionListener(this);
 		msg_enter.addActionListener(this);
 	}
 	
-	// 화면
+	//채팅창 화면
+	public void dp2(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 440);
+		contentPane2 = new JPanel();
+		contentPane2.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane2);
+		contentPane2.setLayout(null);
+		
+		scrollPane.setBounds(12, 10, 406, 346);
+		contentPane2.add(scrollPane);
+		textArea.setEditable(false); // 엑션들추가 에다 넣었더니 반응 없음 해결해주실 수 있는분
+		
+		textField = new JTextField();
+		textField.setBounds(12, 366, 406, 25);
+		contentPane2.add(textField);
+		textField.setColumns(10);
+		textField.addKeyListener(this);
+		
+		msg_enter.setBounds(430, 366, 142, 25);
+		contentPane2.add(msg_enter);
+		
+		JLabel lblNewLabel = new JLabel("접속자");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(430, 8, 142, 25);
+		contentPane2.add(lblNewLabel);
+		
+		scrollPane2.setBounds(430, 31, 142, 322);
+		contentPane2.add(scrollPane2);
+		
+		this.setVisible(true);
+	}
+	
+	//접속 화면
 	public void dp(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 300, 450);
@@ -87,52 +233,67 @@ public class Client extends JFrame implements ActionListener{
 		this.setVisible(true);
 	}
 	
-	public void dp2(){
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 440);
-		contentPane2 = new JPanel();
-		contentPane2.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane2);
-		contentPane2.setLayout(null);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(12, 10, 406, 346);
-		contentPane2.add(textArea);
-		
-		textField = new JTextField();
-		textField.setBounds(12, 366, 406, 25);
-		contentPane2.add(textField);
-		textField.setColumns(10);
-		
-		msg_enter.setBounds(430, 366, 142, 25);
-		contentPane2.add(msg_enter);
-		
-		JLabel lblNewLabel = new JLabel("접속자");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(430, 8, 142, 25);
-		contentPane2.add(lblNewLabel);
-		
-		JList list = new JList();
-		list.setBounds(430, 31, 142, 322);
-		contentPane2.add(list);
-		
-		this.setVisible(true);
-	}
-
+	// 메인
 	public static void main(String[] args) {
 		
 		new Client();
 	}
-
+	
+	
+	// 버튼 엑션
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == sv_join){
-			System.out.println("서버 접속 버튼");
+			//System.out.println("서버 접속 버튼");
+			if(tf_port.getText().trim().equals("")){
+			}else if(tf_nickname.getText().trim().equals("")){
+			}else{
+				Ip = tf_ip.getText().trim();
+				port = Integer.parseInt(tf_port.getText().trim());
+				nickname = tf_nickname.getText().trim();			
+				Start();
+				dp2(); //채팅창 화면 // textArea.setEditable(false); 안되길래 였다 쳐넣음
+				
+			}
 		}
 		else if(e.getSource() == msg_enter){
-			System.out.println("메세지 전송 버튼");
+			//System.out.println("메세지 전송 버튼");
+			if(!textField.getText().equals("")){
+				String msg = textField.getText().trim();
+				SendMsg(msg);
+				textField.setText("");
+				textField.requestFocus();
+			}
 		}
+	}
+	
+	
+	// 키입력
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == 10){
+			//System.out.println("엔터키");
+			if(!textField.getText().equals("")){
+				String msg = textField.getText().trim();
+				SendMsg(msg);
+				textField.setText("");
+				textField.requestFocus();
+			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
--- Server.java
+++ Server.java
@@ -8,11 +8,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -27,11 +29,156 @@ public class Server extends JFrame implements ActionListener{
 	private JButton sv_end = new JButton("서버 종료");
 	private JLabel label = new JLabel("포트 번호");
 	
+	private ServerSocket svsc;
+	private Socket sc;
+	private int port;
+	Vector user = new Vector();
+	private StringTokenizer st;
+	
+	public void Start(){
+		try {
+			svsc = new ServerSocket(port); // 서버 열기
+			
+			if(svsc != null){
+				access();
+			}
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "사용중인 포트입니다.", "알림", JOptionPane.ERROR_MESSAGE);
+		}
+		
+	}
+	
+	public void access(){
+		Thread th = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while(true){
+					try {
+						textArea.append("사용자 접속 대기중...\n");
+						sc = svsc.accept();
+						
+						UserInfo user = new UserInfo(sc);
+						
+						user.start();
+						
+					} catch (IOException e) {
+						break;
+					}
+				}
+			}
+		});
+		th.start();
+		
+	}
+	
+	class UserInfo extends Thread{
+		
+		private InputStream is;
+		private OutputStream os;
+		private DataInputStream dis;
+		private DataOutputStream dos;
+		
+		private Socket user_sc;
+		private String Nickname;
+		
+		UserInfo(Socket soc){
+			this.user_sc = soc; // 유저 소켓
+			
+			userset();
+		}
+		
+		public void userset(){
+			try {
+				is = user_sc.getInputStream();
+				os = user_sc.getOutputStream();
+				dis = new DataInputStream(is);
+				dos = new DataOutputStream(os);
+				
+				Nickname = dis.readUTF();
+				textArea.append(Nickname+"님 접속\n");
+				
+				BroadCast("NewUser/"+Nickname);
+				
+				for(int i = 0;i< user.size();i++){
+					UserInfo User = (UserInfo)user.elementAt(i);
+					SendMsg("OldUser/"+User.Nickname);
+				}
+				
+				user.add(this);
+				
+				BroadCast("setListData/1");
+				
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "스티림 에러", "알림", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		
+		public void run(){
+			while(true){
+				try {
+					String msg = dis.readUTF();
+					
+					st = new StringTokenizer(msg, " ");
+					
+					String pt = st.nextToken();
+					
+					if(pt.equals("/ㄱ") || pt.equals("/귓속말") || pt.equals("/귓")){
+						String nname = st.nextToken();
+						String Msg = "";
+						while (st.hasMoreTokens()) {
+							Msg += " "+st.nextToken();
+						}
+						for(int i = 0;i< user.size();i++){
+							UserInfo User = (UserInfo)user.elementAt(i);
+							if(User.Nickname.equals(nname)){
+								User.SendMsg("Whisper/"+Nickname+":"+Msg);
+							}
+						}
+						SendMsg("Whisper/"+nname+"님에게:"+Msg);
+					}else{
+						BroadCast(Nickname+"/"+msg);
+					}
+				} catch (IOException e) {
+					try {
+						dos.close();
+						dis.close();
+						user_sc.close();
+						user.remove(this);
+						BroadCast("UserOut/"+Nickname);
+						BroadCast("setListData/1");
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					textArea.append(Nickname+"님 접속 종료\n");
+					break;
+				}
+				
+			}
+		}
+		
+		public void BroadCast(String str){
+			for(int i = 0;i< user.size();i++){
+				UserInfo User = (UserInfo)user.elementAt(i);
+				User.SendMsg(str);
+			}
+		}
+		
+		public void SendMsg(String msg){
+			try {
+				dos.writeUTF(msg);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	Server(){
 		dp(); // 화면
 		actions();
 	}
 	
+	// 엑션들추가
 	public void actions(){
 		sv_start.addActionListener(this);
 		sv_end.addActionListener(this);
@@ -51,6 +198,7 @@ public class Server extends JFrame implements ActionListener{
 		
 		sv_end.setBounds(210, 350, 150, 50);
 		contentPane.add(sv_end);
+		sv_end.setEnabled(false);
 		
 		label.setFont(new Font("굴림", Font.PLAIN, 18));
 		label.setBounds(50, 310, 100, 30);
@@ -63,24 +211,42 @@ public class Server extends JFrame implements ActionListener{
 		
 		textArea.setBounds(12, 10, 370, 290);
 		contentPane.add(textArea);
+		textArea.setEditable(false);
 		
 		this.setVisible(true);
 	}
-
+	
+	// 메인
 	public static void main(String[] args) {
 		
 		new Server();
 		
 	}
 
+	// 버튼
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getSource() == sv_start){
-			System.out.println("서버 실행 버튼");
+			//System.out.println("서버 실행 버튼");
+			port = Integer.parseInt( tf_Port.getText().trim());
+			Start();
+			sv_start.setEnabled(false);
+			tf_Port.setEditable(false);
+			sv_end.setEnabled(true);
 		}
 		else if(e.getSource() == sv_end){
-			System.out.println("서버 종료 버튼");
+			//System.out.println("서버 종료 버튼");
+			try {
+				svsc.close();
+				user.removeAllElements();
+				textArea.append("서버종료\n");
+				sv_end.setEnabled(false);
+				tf_Port.setEditable(true);
+				sv_start.setEnabled(true);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		}
 		
 	}
